dbded4008163545d16d469c7d0a6ad0c7a701628
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-03 21:00:58 +0300

    db add

--- models/testimonialsModel.js
+++ models/testimonialsModel.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose");
+
+const testimonialSchema = mongoose.Schema({
+  name: String,
+  testimonial: String,
+});
+
 const sendTestimonial = async (body) => {};
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+run();
+async function run() {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+    console.log("Database connection successful");
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
