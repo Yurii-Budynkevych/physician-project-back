5271801f9ab9cfe9f3246bf825108af1896aad6d
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-04 23:41:56 +0300

    fix

--- routes/API/mailer.js
+++ routes/API/mailer.js
@@ -1,8 +1,51 @@
 const express = require("express");
-const { sendEmail } = require("../../controllers/mailerControllers");
+const nodemailer = require("nodemailer");
+const Joi = require("joi");
+// dantax13@gmail.com
 
 const router = express.Router();
 
-router.post("/", sendEmail);
+router.post("/", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(1).max(30).trim().required(),
+    email: Joi.string().email().trim().required(),
+    tel: Joi.string().min(5).max(30).trim().required(),
+    text: Joi.string().min(1).max(3000).trim().required(),
+  });
+
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ message: "bad request", info: validationResult.error });
+  }
+
+  const { name, email, tel, text } = req.body;
+
+  const message = {
+    from: process.env.MAILER_API_USER,
+    to: "jyrka95@gmail.com",
+    subject: "Новий пацієнт ✔",
+    text: `${name} записався на консультацію!
+     Контактні дані: пошта - ${email}, телефон - ${tel}.
+     Суть питання - ${text}.
+     Зв'яжіться з пацієнтом якнайшвидше!`,
+  };
+
+  const config = {
+    service: "gmail",
+    auth: {
+      user: process.env.MAILER_API_USER,
+      pass: process.env.MAILER_API_KEY,
+    },
+  };
+
+  const transporter = nodemailer.createTransport(config);
+
+  transporter
+    .sendMail(message)
+    .then(() => res.status(201).json({ message: "email sent" }))
+    .catch((error) => res.status(500).json({ message: error }));
+});
 
 module.exports = router;
--- routes/api/testimonials.js
+++ routes/api/testimonials.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Joi = require("joi");
 const { TestimonialsDbModel } = require("../../models/testimonialsModel");
 
 const router = express.Router();
@@ -13,7 +14,20 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(1).max(30).trim().required(),
+    testimonial: Joi.string().min(1).max(3000).trim().required(),
+  });
+
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ message: "bad request", info: validationResult.error });
+  }
+
   const { name, testimonial } = req.body;
+
   try {
     const result = await TestimonialsDbModel.create({ name, testimonial });
     res.json(result);
