3e34c31ede4d73a12543e51627266d507a2fe419
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-05 17:49:33 +0300

    fix

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ async function run() {
     });
   } catch (error) {
     console.error(error);
-    process.exit(1);
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
   }
 }
