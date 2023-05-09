c0165ce33929c24fa7138d351c9967eaa15ae9d0
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-09 17:54:45 +0300

    fix

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ async function run() {
     await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
     console.log("Database connection successful");
 
-    app.listen(3000, () => {
+    app.listen(3001, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   } catch (error) {
     console.error(error);
 
-    app.listen(3000, () => {
+    app.listen(3001, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   }
